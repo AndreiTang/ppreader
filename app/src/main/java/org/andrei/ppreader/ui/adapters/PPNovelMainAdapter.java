fabd46649b2504f16de027a517749c641fc3ca91
qi-feng.tang@hp.com <qi-feng.tang@hp.com>
2018-09-14 00:37:57 +0800

    add mainfragement

---
+++ app/src/main/java/org/andrei/ppreader/ui/adapters/PPNovelMainAdapter.java
@@ -0,0 +1,30 @@
+package org.andrei.ppreader.ui.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import org.andrei.ppreader.ui.fragments.PPNovelListFragment;
+import org.andrei.ppreader.ui.fragments.PPNovelSearchFragment;
+
+public class PPNovelMainAdapter extends FragmentPagerAdapter {
+
+
+    public PPNovelMainAdapter(FragmentManager fm) {
+        super(fm);
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return m_fragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    Fragment[] m_fragments = {new PPNovelListFragment(),new PPNovelSearchFragment()};
+
+}
--- app/src/main/java/org/andrei/ppreader/ui/fragments/PPNovelMainFragment.java
+++ app/src/main/java/org/andrei/ppreader/ui/fragments/PPNovelMainFragment.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.andrei.ppreader.R;
+import org.andrei.ppreader.ui.adapters.PPNovelMainAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +37,11 @@ public class PPNovelMainFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         this.getActivity().findViewById(android.R.id.content).setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        PPNovelMainAdapter adapter = new PPNovelMainAdapter(this.getChildFragmentManager());
+        ViewPager vp = (ViewPager)getView().findViewById(R.id.main_viewpager);
+        vp.setAdapter(adapter);
+
     }
 
 
